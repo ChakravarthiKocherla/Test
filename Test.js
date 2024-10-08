1cb7c4073ad97cf299372a19fe7dfff0e46934f3
ChakravarthiKocherla <chakrkocherla321@gmail.com>
2024-10-08 17:01:55 +0530

    single number

--- Test.js
+++ Test.js
@@ -3,7 +3,9 @@ const add = require('./calculator');
 function testAdd() {
 
     console.assert(add("") === 0, "Test failed: empty string should return 0");
-    
+
+    console.assert(add("1") === 1, "Test failed: '1' should return 1");
+
     console.log("All tests passed!");
     
 }
